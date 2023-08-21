5b557f8c3d1a3340cf000afcfcf48a0b5164bd9d
btani <beatricetnii@gmail.com>
2023-08-21 16:08:44 +0200

    push prova 21/08

--- src/exec_red.c
+++ src/exec_red.c
@@ -15,8 +15,14 @@
 /* I cast a void mi servivano solo per vedere se tutto compilava correttamente,
 	si possono tranquillamente togliere una volta che si inizia a lavorare
 	effettivamente sulle funzioni */
-int	ft_exec_red(t_mini *shell, char **mtx)
+int	ft_exec_red(t_mini *shell, t_token tkn)
 {
-	(void)shell;
-	(void)mtx;
+	int	count;
+	int	exitcode;
+	
+	count = 0;
+	while (tkn->red && tkn->red[count++])
+	{
+		if (tkn->red[count] == 0)
+	}
 }
